237f37f2284dfa5e1565491f8885ddafd71b026a
ryanbliss <ryan@hashtaggy.co>
2018-03-10 17:56:41 -0600

    Initial Commit

---
+++ Sample JSON to UI/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Sample JSON to UI
+//
+//  Created by Ryan Bliss on 3/10/18.
+//  Copyright © 2018 Ryan Bliss. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
